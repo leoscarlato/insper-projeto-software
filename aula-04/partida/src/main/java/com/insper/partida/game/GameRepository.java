9c38963b0de15b2ff6fced59457dc75d5d07b2ff
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2023-08-28 14:43:12 -0300

    ta tudo funfando aparentemente

--- aula-04/partida/src/main/java/com/insper/partida/game/GameRepository.java
+++ aula-04/partida/src/main/java/com/insper/partida/game/GameRepository.java
@@ -3,12 +3,12 @@ package com.insper.partida.game;
 import com.insper.partida.equipe.Team;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GameRepository extends JpaRepository<Game, Integer> {
+public interface GameRepository extends MongoRepository<Game, String> {
     Game findByIdentifier(String identifier);
 
     Page<Game> findByHomeAndAway(Team tHome, Team tAway, Pageable pageable);
